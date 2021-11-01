8192e188ec34d32e7ae7b548c8ddd5895aaa3bf6
farhan <reyhanreynold11@gmail.com>
2021-11-02 00:43:06 +0700

    Tugas 1 Js

---
+++ tugas1.js
@@ -0,0 +1,5 @@
+var namaDepan = "Muhammad";
+let namaBelakang = "Farhan";
+const umur = 18;
+
+console.log ("nama depan saya adalah", namaDepan, "dan nama belakang saya adalah", namaBelakang, "sekarang saya berusia", umur );
